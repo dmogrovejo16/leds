b2c336b87e1dc3f884b78211e7a4098e0b4a99f9
MVMATHIAS <130251210+dmogrovejo16@users.noreply.github.com>
2024-02-07 12:24:06 -0500

    Add files via upload

---
+++ IoT-Ionic/src/environments/environment.ts
@@ -0,0 +1,25 @@
+import { initializeApp } from "firebase/app";
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+  apiKey: "AIzaSyCj_-IvCkVsNLn27cKXP3XY7c5uM8YK0P4",
+  authDomain: "iot-ionic-87e9b.firebaseapp.com",
+  projectId: "iot-ionic-87e9b",
+  storageBucket: "iot-ionic-87e9b.appspot.com",
+  messagingSenderId: "367241163197",
+  appId: "1:367241163197:web:286e49f831500dc64aa2d4"
+}
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
